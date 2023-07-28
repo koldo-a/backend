3d97263ca8252578170f17c4cd0380854d9d57dc
koldoarretxea <koldo.arretxea@gmail.com>
2023-07-28 11:20:45 +0200

    d

--- main.py
+++ main.py
@@ -208,4 +208,4 @@ def get_user_email(user_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=6932))
