827144f711540487d5850fa775183706a94f31a5
Koldo Arretxea <118269264+koldo-a@users.noreply.github.com>
2023-08-03 17:59:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -208,4 +208,4 @@ def get_user_email(user_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000))
